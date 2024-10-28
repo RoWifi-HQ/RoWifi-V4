9033461ebe4a1a462b73f025aa27d3a796f24a4f
AsianIntel <gautam.abhyankar@gmail.com>
2024-10-28 14:45:55 -0700

    update linked_users on account switch

--- rowifi/src/commands/user/accounts/switch.rs
+++ rowifi/src/commands/user/accounts/switch.rs
@@ -86,6 +86,10 @@ Oh no! An account with the name `{}` does not seem to exist. Ensure you have spe
             &[&ctx.author_id, &map],
         )
         .await?;
+    bot.database.execute(
+        "INSERT INTO linked_users(roblox_id, user_id, guild_id) ON CONFLICT(guild_id, user_id) SET roblox_id = $1", 
+        &[&roblox_user.id, &user.user_id, &ctx.guild_id]
+    ).await?;
 
     let message = format!(
         r#"
