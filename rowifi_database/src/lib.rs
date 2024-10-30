c2cd7f5e54afcb804e395bbd9d6b12e853804bde
Gautam Abhyankar <gautam.abhyankar@gmail.com>
2024-10-29 18:03:30 -0700

    Update max connections

--- rowifi_database/src/lib.rs
+++ rowifi_database/src/lib.rs
@@ -26,7 +26,7 @@ impl Database {
         let postgres_config = TokioPostgresConfig::from_str(connection_string).unwrap();
         let manager = Manager::new(postgres_config, NoTls);
         let pool = Pool::builder(manager)
-            .max_size(16)
+            .max_size(128)
             .runtime(Runtime::Tokio1)
             .recycle_timeout(Some(Duration::from_secs(30)))
             .create_timeout(Some(Duration::from_secs(30)))
